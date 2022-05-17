e537e59e04ace120f853cc76fd1e9643564b5d20
Gabriel Valdivia <gabriel.softdev@gmail.com>
2022-05-17 00:09:33 -0300

    Chain of Responsibility

--- src/CalculadoraDeDescontos.php
+++ src/CalculadoraDeDescontos.php
@@ -6,9 +6,13 @@ class CalculadoraDeDescontos
 {
     public function calculaDescontos(Orcamento $orcamento): float
     {
+
         if ($orcamento->quantidadeItens > 5)
             return $orcamento->valor * 0.1;
-            
+
+        if ($orcamento->valor > 500)
+            return $orcamento->valor * 0.05;
+
         return 0;
     }
 }
---
+++ src/classes/descontos/DescontoMaisDe500Gastos.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Alura\DesignPattern\Classes\Descontos;
+
+class DescontoMaisDe500Gastos {
+
+}
